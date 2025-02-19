70012e547403bbcdf21ba3394f699f49ecfc8bbe
Melody Oviedo Morales <moviedomorales@estudiantes.unsam.edu.ar>
2025-02-19 08:49:17 -0300

    Creo componente SectionTitle

---
+++ src/components/common/SectionTitle.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function SectionTitle({title}:{title:string}) {
+  const capitalizeText = (text:string) => {
+    return text.toUpperCase()
+  }
+
+  return (
+    <h2 
+      style={{textAlign: 'center',
+              marginBottom: '.5 rem',
+              fontSize: '1.25rem', // Tamaño similar al de Typography variant="h5"
+              fontWeight: 'bold'}}>{capitalizeText(title)}</h2>
+  )
+}
