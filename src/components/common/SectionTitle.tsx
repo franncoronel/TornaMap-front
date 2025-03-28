03c78202fd0f203ab3e35fd9ef11c2740734d111
franncoronel <franncoronel@gmail.com>
2025-03-28 19:22:59 -0300

    Agrego children al componente sectiontitle y formateo

--- src/components/common/SectionTitle.tsx
+++ src/components/common/SectionTitle.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import { Box } from '@mui/material'
+import React, { ReactNode } from 'react'
 
-export default function SectionTitle({title}:{title:string}) {
-  const capitalizeText = (text:string) => {
+export default function SectionTitle({ title, children }: { title: string, children?: ReactNode }) {
+  const capitalizeText = (text: string) => {
     return text.toUpperCase()
   }
 
   return (
-    <h2 
-      style={{textAlign: 'center',
-              marginBottom: '.5 rem',
-              fontSize: '1.25rem', // Tamaño similar al de Typography variant="h5"
-              fontWeight: 'bold'}}>{capitalizeText(title)}</h2>
+    <Box>
+    <h2
+      style={{
+        textAlign: 'center',
+        fontSize: '1.25rem', // Tamaño similar al de Typography variant="h5"
+        fontWeight: 'bold'
+      }}>{capitalizeText(title)}</h2>
+      {children}
+      </Box>
   )
 }
