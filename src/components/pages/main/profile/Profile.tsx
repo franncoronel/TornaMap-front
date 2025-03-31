2a5e3699a39c5b0db6c6838fa676caca27210f8b
Francisco Coronel <ffranncoronel@gmail.com>
2025-03-31 15:30:35 -0300

    Elimino título

--- src/components/pages/main/profile/Profile.tsx
+++ src/components/pages/main/profile/Profile.tsx
@@ -5,18 +5,14 @@ import SectionTitle from "@/components/common/SectionTitle"
 export default function Profile() {
   return (
     <main>
-      <Box sx={{ flexShrink: 0, paddingTop: '2rem' }}>
-        <SectionTitle title="perfil" />
-      </Box>
       <Stack
+        flexGrow='1'
         direction='column'
         justifyContent='center'
         alignItems='center'
         spacing={6}
         padding={3}
       >
-        {/* <Typography variant='h5' fontWeight='bold'>PERFIL</Typography> */}
-        
         <ProfileButton>Agregar Tipo de Aula</ProfileButton>
         <ProfileButton>Editar Aula</ProfileButton>
         <ProfileButton>Habilitar Solicitud Clase</ProfileButton>
