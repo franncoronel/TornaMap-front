37b7f821578f0bcb6693a8164af8e3bde92d8632
Francisco Coronel <ffranncoronel@gmail.com>
2025-03-31 20:12:34 -0300

    Planteo posible solucion al renderizado dependiendo del tamaño de la pantalla

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,21 @@ import CssBaseline from '@mui/material/CssBaseline'
 import { AuthProvider } from "@/context/AuthContext.tsx"
 import Nav from './components/common/Nav/Nav'
 import { routes } from '@/routes'
+import { useMediaQuery, useTheme } from '@mui/material'
+import Header from './components/common/Header'
 
 function App() {
-
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
 
   return (
     <>
       <CssBaseline />
       <AuthProvider>
         <BrowserRouter>
+          {!isMobile && <Header />}
           <RoutesWrapper />
-          <Nav />
+          {isMobile && <Nav />}
         </BrowserRouter>
       </AuthProvider>
     </>
