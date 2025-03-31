f31634f6a99c301613b4ace89110849214712af7
Francisco Coronel <ffranncoronel@gmail.com>
2025-03-31 15:30:10 -0300

    Elimino definición de children

--- src/components/common/SectionTitle.tsx
+++ src/components/common/SectionTitle.tsx
@@ -1,20 +1,19 @@
-import { Box } from '@mui/material'
-import React, { ReactNode } from 'react'
+import React from 'react'
 
-export default function SectionTitle({ title, children }: { title: string, children?: ReactNode }) {
+export default function SectionTitle({ title }: { title: string }) {
   const capitalizeText = (text: string) => {
     return text.toUpperCase()
   }
 
   return (
-    <Box>
     <h2
       style={{
         textAlign: 'center',
         fontSize: '1.25rem', // Tamaño similar al de Typography variant="h5"
         fontWeight: 'bold'
-      }}>{capitalizeText(title)}</h2>
-      {children}
-      </Box>
+      }}
+    >
+      {capitalizeText(title)}
+    </h2>
   )
 }
