b0008083cd8ffb4ac414dd593d7c3d55a38927ad
Francisco Coronel <ffranncoronel@gmail.com>
2025-04-01 14:00:02 -0300

    Elimino reglas de css innecesarias e identifico aquellas cruciales

--- src/components/common/Nav/Nav.tsx
+++ src/components/common/Nav/Nav.tsx
@@ -22,18 +22,54 @@ export default function Nav() {
     )
   }
   return (
-    <AppBar className="footer-nav-bar" position="sticky" color="primary" sx={{ top: 'auto', bottom: 0, height: '10vh', display:'flex', justifyContent:'center', alignItems:'center' }} enableColorOnDark>
-      <Toolbar sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%', height: '100%'}}>
-        <IconButton onClick={() => navigate("/")} color='secondary' className={isActive('/')?'active':''} aria-label='Buscar'>
+    <AppBar
+      className="footer-nav-bar" // Particular al footer
+      position="sticky"
+      color="primary"
+      sx={{
+          top: 'auto',  // Particular al footer
+          bottom: 0,    // Particular al footer
+          height: '10vh'// Particular al footer
+      }}
+      enableColorOnDark
+    >
+      <Toolbar
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          height: '100%'
+        }}
+      >
+        <IconButton
+          onClick={() => navigate("/")}
+          color='secondary'
+          className={isActive('/')?'active':''}
+          aria-label='Buscar'
+        >
           <HouseSimple size={32} alt='Inicio'/>
         </IconButton>
-        <IconButton onClick={() => navigate("/buscar")} color='secondary' className={isActive('/buscar')?'active':''} aria-label='Buscar'>
+        <IconButton
+          onClick={() => navigate("/buscar")}
+          color='secondary'
+          className={isActive('/buscar')?'active':''}
+          aria-label='Buscar'
+        >
           <MagnifyingGlass size={32} alt='Buscar' />
         </IconButton>
-        <IconButton onClick={() => navigate("/mapa/tornavias-subsuelo")} color='secondary' className={isActive('/mapa')?'active':''} aria-label='Inicio'>
+        <IconButton
+          onClick={() => navigate("/mapa/tornavias-subsuelo")}
+          color='secondary'
+          className={isActive('/mapa')?'active':''}
+          aria-label='Inicio'
+        >
           <MapTrifold size={32} alt='Mapa' />
         </IconButton>
-        <IconButton onClick={() => navigate("/perfil")} color='secondary' className={isActive(["/perfil", "/ingresar"])?'active':''} aria-label='Perfil'>
+        <IconButton
+          onClick={() => navigate("/perfil")}
+          color='secondary'
+          className={isActive(["/perfil", "/ingresar"])?'active':''}
+          aria-label='Perfil'
+        >
           {isAuthenticated && <UserCircle size={32} alt='Perfil' />}
           {!isAuthenticated && <SignIn size={32} alt='Iniciar Sesión' />}
         </IconButton>
