a0caa521bc32a8ce667c878c96de0960a491c647
Francisco Coronel <ffranncoronel@gmail.com>
2025-04-01 21:43:08 -0300

    Refactorizo el estilado del nav para volverlo más responsive

--- src/components/common/Nav/Nav.tsx
+++ src/components/common/Nav/Nav.tsx
@@ -5,22 +5,13 @@ export default function Nav() {
 
   return (
     <AppBar
-      className="footer-nav-bar" // Particular al footer
+      className="appbar" // Particular al footer
       position="sticky"
       color="primary"
-      sx={{
-          top: 'auto',  // Particular al footer
-          bottom: 0,    // Particular al footer
-          height: '10vh'// Particular al footer
-      }}
       enableColorOnDark
     >
       <Toolbar
-        sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          height: '100%'
-        }}
+        className="toolbar"
       >
         <NavigationButtons />
       </Toolbar>
