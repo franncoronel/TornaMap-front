622ebbfa99433f366e3311e40b83c86c211043e2
Francisco Coronel <ffranncoronel@gmail.com>
2025-04-14 12:46:15 -0300

    Adapto vista Profile a la solución de estilos general

--- src/components/pages/main/profile/Profile.tsx
+++ src/components/pages/main/profile/Profile.tsx
@@ -1,24 +1,20 @@
-import { Box, Stack} from "@mui/material"
+import { Stack} from "@mui/material"
 import { ProfileButton } from "./ProfileButton"
-import SectionTitle from "@/components/common/SectionTitle"
 
 export default function Profile() {
   return (
-    <main>
-      <Stack
+    <Stack
         flexGrow='1'
         direction='column'
         justifyContent='center'
         alignItems='center'
         spacing={6}
-        padding={3}
-      >
-        <ProfileButton>Agregar Tipo de Aula</ProfileButton>
-        <ProfileButton>Editar Aula</ProfileButton>
-        <ProfileButton>Habilitar Solicitud Clase</ProfileButton>
-        <ProfileButton>Agregar Clase</ProfileButton>
-        <ProfileButton>Editar Clase</ProfileButton>
-      </Stack>
-    </main>
+    >
+      <ProfileButton>Agregar Tipo de Aula</ProfileButton>
+      <ProfileButton>Editar Aula</ProfileButton>
+      <ProfileButton>Habilitar Solicitud Clase</ProfileButton>
+      <ProfileButton>Agregar Clase</ProfileButton>
+      <ProfileButton>Editar Clase</ProfileButton>
+    </Stack>
   )
 }
