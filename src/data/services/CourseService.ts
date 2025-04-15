4d2c9b1dbc52a5629cb9cdd3ae367b8b617f1c84
agusnarvaez <agus.narvaez@outlook.com>
2025-04-15 13:22:59 -0300

    Reubico course Service

---
+++ src/data/services/CourseService.ts
@@ -0,0 +1,22 @@
+import axios from "axios"
+import { ICourse, ICourseList } from "../domain/Course"
+import { ServiceInterface } from "./ServiceInterface"
+import { REST_API_URL } from "../constants"
+import { Response } from "../domain/Response"
+export class CourseService implements ServiceInterface {
+    async getAll(query?: string): Promise<Response<ICourseList[]>> {
+        const url = query ? `${REST_API_URL}/courses?query=${query}` : `${REST_API_URL}/courses`
+        const courseDTOs = await axios.get<Response<ICourseList[]>>(url)
+        console.log(courseDTOs)
+        const courses = courseDTOs.data
+        return courses
+    }
+
+    async getById(id: string | number): Promise<Response<ICourse>> {
+         const courseDTO = await axios.get<Response<ICourse>>(`${REST_API_URL}/courses/${id}`)
+         const course = courseDTO.data
+         return course
+    }
+}
+
+export const courseService = new CourseService()
