658d9267ae952cbf417235b248f53784905d318b
agusnarvaez <agus.narvaez@outlook.com>
2025-04-15 19:37:19 -0300

    Creo LoaderContext

---
+++ src/context/LoaderContext.tsx
@@ -0,0 +1,45 @@
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  FC,
+  ReactNode,
+  Dispatch,
+  SetStateAction
+} from 'react'
+import CircularProgress from '@mui/material/CircularProgress'
+import { Box } from '@mui/material'
+
+interface LoaderContextProps {
+  setLoader: Dispatch<SetStateAction<boolean>>
+}
+
+const LoaderContext = createContext<LoaderContextProps | undefined>(undefined)
+
+export const LoaderProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const [loader, setLoader] = useState<boolean>(false)
+
+  // Si querés también limpiar la notificación al montar el provider:
+  useEffect(() => {}, [])
+
+  return (
+    <LoaderContext.Provider value={{ setLoader }}>
+      {children}
+      {loader && (
+        <Box className="loader-container">
+          <span className="loader-unsam">UNSAM</span>
+          <CircularProgress className="loader" size={250} />
+        </Box>
+      )}
+    </LoaderContext.Provider>
+  )
+}
+
+export const useLoader = () => {
+  const context = useContext(LoaderContext)
+  if (!context) {
+    throw new Error('useLoader debe ser usado dentro de un LoaderProvider')
+  }
+  return context
+}
