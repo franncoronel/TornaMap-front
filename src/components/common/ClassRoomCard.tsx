01ce4f5ffaf783aa516f77896a6102bb3e8753c6
agusnarvaez <agus.narvaez@outlook.com>
2025-04-15 00:32:18 -0300

    Actualizo implementación de ClassRoomCard

--- src/components/common/ClassRoomCard.tsx
+++ src/components/common/ClassRoomCard.tsx
@@ -1,45 +1,93 @@
+import { ICourse } from "@/data/domain/Course";
+import { ISchedule } from "@/data/domain/Schedule";
+/* import { EventAvailableTwoTone } from "@mui/icons-material"; */
 import { CardActionArea, CardContent, Typography, Card, Box, Divider, Tooltip} from "@mui/material";
-import { MapPin,Clock,User,BookOpenText, Building, Laptop} from '@phosphor-icons/react'
+import { MapPin,Clock,User,BookOpenText, Building, Laptop, ArrowsClockwise} from '@phosphor-icons/react'
 
 
 interface ClassRoomCardProps{
-  name : string
-  // classroomType : string
-  commission : string
-  classroom : string
-  building : string
-  teacher : string[]
-  careers : string[]
-  schedules : string
+  course? : ICourse
+  schedule?: ISchedule
   onClick?: () => void
   viewType: string//'standard' | 'modal'
-  mode: 'presencial' | 'virtual'
 }
 
 
-export default function ClassRoomCard({name,
-                                      commission,
-                                      classroom,
-                                      building,
-                                      teacher,
-                                      careers,
-                                      schedules,
-                                      viewType,
-                                      mode,
-                                      onClick}:ClassRoomCardProps)
+export default function ClassRoomCard({
+    course,
+    onClick,
+    viewType,
+    schedule
+  }:ClassRoomCardProps)
 {
-  const formattedMode = mode.charAt(0).toUpperCase() + mode.slice(1).toLowerCase()
+  //const formattedMode = mode.charAt(0).toUpperCase() + mode.slice(1).toLowerCase()
 
 {/* //TODO: COMENTARIOS DE MEJORAS
 // * - Cambiar el formato de horario
 // *     Horario:
-// *        Lunes: 08:00 - 10:00 
+// *        Lunes: 08:00 - 10:00
 // *        Martes: 18:00 - 22:00
 
 // * - Agregar el botón de edición (SpeedDialEditActions)
 // * - Agregar al modal los distintos edificios y aulas (en el caso de que se curse en distintos edificios)
 // * - Crear el modal para editar la materia
 */}
+
+  const isVirtual = ()=>{
+    if(course){
+      return course?.events.some((event) => event.schedules.some((schedule) => schedule.isVirtual))
+    }else{
+      return schedule?.isVirtual
+    }
+  }
+  const isPresential = () => {
+    if(course){
+      return course?.events.some((event) => event.schedules.some((schedule) => !schedule.isVirtual))
+    }else{
+      return !schedule?.isVirtual
+    }
+  }
+
+  const classroom = () => {
+    if(course){
+      return course?.events.map((event) => event.schedules.map((schedule) => schedule.classroom?.name).join(', ')).join(', ')
+    }else{
+      return schedule?.classroom?.name
+    }
+  }
+
+  const building = () => {
+    if(course){
+      return course?.events.map((event) => event.schedules.map((schedule) => schedule.classroom?.building?.name).join(', ')).join(', ')
+    }else{
+      return schedule?.classroom?.building?.name
+    }
+  }
+
+  const timeSchedule = () => {
+    if(course){
+      return course?.events.map((event) => event.schedules.map((schedule) => `${schedule.startTime} - ${schedule.endTime}`).join(', ')).join(', ')
+    }else{
+      return schedule?.startTime + ' - ' + schedule?.endTime
+    }
+  }
+
+  const professors = () => {
+    if(course){
+      return course.events.map((event)=>event.schedules.map(schedule=>schedule.professors.map(professor=>professor).join(' - ')))
+    }else{
+      return schedule?.professors.map(professor => professor).join(', ')
+    }
+  }
+
+  const programs = () => {
+    if(course){
+      return course?.programs.map(program=>program).join(', ')
+    }else{
+      return null
+    }
+  }
+
 
   return (
     <Box  sx={{
@@ -62,15 +110,15 @@ export default function ClassRoomCard({name,
           <CardContent sx={{ backgroundColor: '#f5f5f5', borderRadius: 3 }}>
           <Box  sx={{ display: 'grid',gridTemplateRows: 'auto auto',justifyItems: 'center',
                       maxWidth: '100%',overflow: 'hidden',mb: 0.5,}}>
-            <Tooltip title={name} arrow placement="top">
+            <Tooltip title={course?.name} arrow placement="top">
               <Typography variant="h5" sx={{ color: '#333',fontWeight: 'bold', whiteSpace: 'nowrap',
                                               maxWidth: '100%', overflow: 'hidden', textOverflow: 'ellipsis',}}>
-                {name}
+                {course?.name}
               </Typography>
             </Tooltip>
             <Typography variant="h5" sx={{color: '#333', fontWeight: 'bold',}}>
-              {commission}
-            </Typography> 
+              {course?.events.map((event) => event?.name).join(', ')}
+            </Typography>
           </Box>
 
           <Divider sx={{ mb: .5 }} />
@@ -82,7 +130,7 @@ export default function ClassRoomCard({name,
                       maxHeight: '90vh',
                       overflowY: 'auto',mb: 0.5,
                       rowGap: 1,}}>
-            
+
             {/* Aula y Edificio */}
             {viewType === "standard" && (
               <>
@@ -92,11 +140,11 @@ export default function ClassRoomCard({name,
                 <Box sx={{display: 'flex',flexDirection: 'column', maxWidth: '100%',overflow: 'hidden'}}>
                   <Typography variant="body2" sx={{ color: '#666',display: 'block', textAlign: 'left', whiteSpace: 'nowrap',
                                               maxWidth: '100%', overflow: 'hidden', textOverflow: 'ellipsis',}}>
-                    Aula: {classroom}
+                    Aula: {classroom()}
                   </Typography>
                   <Typography variant="body2" sx={{ color: '#666',display: 'flex', textAlign: 'left',}}>
-                    Edificio: {building}
-                  </Typography>       
+                    Edificio: {building()}
+                  </Typography>
                 </Box>
               </>
             )}
@@ -104,7 +152,7 @@ export default function ClassRoomCard({name,
             {/* Profesor */}
             <User size={24} color="#1976d2" />
             <Box  sx={{maxWidth: '100%',overflow: 'hidden'}}>
-              <Tooltip title={teacher.join(' - ')} arrow>
+              <Tooltip title={professors()} arrow>
                 <Typography
                   variant="body2"
                   sx={{ color: '#666',
@@ -113,46 +161,45 @@ export default function ClassRoomCard({name,
                         textOverflow: 'ellipsis',
                         display: 'block',
                         textAlign: 'left',}}>
-                  Profesor: {teacher.join(' - ')}
+                  Profesor: {professors()}
                 </Typography>
               </Tooltip>
             </Box>
-            
+
             {/* Modalidad */}
-            {mode === 'presencial' ? (
-              <Building size={24} color="#1976d2" />
-            ) : mode === 'virtual' ? (
-              <Laptop size={24} color="#1976d2" />
-            ) : (
-              <Building size={24} color="#1976d2" />
-            )}
+              {isPresential() && !isVirtual() && <Building size={24} color="#1976d2" />}
+              {isVirtual() && !isPresential() && <Laptop size={24} color="#1976d2" />}
+              {isPresential() && isVirtual() && <ArrowsClockwise color="#1976d2" size={24} />}
+
             <Typography variant="body2" sx={{ color: '#666',display: 'flex', textAlign: 'left', }}>
-              Modalidad: {formattedMode}
+              Modalidad: {isVirtual()?'Virtual':''} {(isVirtual()&&isPresential()?' - ':'')} {isPresential()?'Presencial':''}
             </Typography>
-            
+
             {/* Horario */}
             <Clock size={24} color="#1976d2" />
             <Typography variant="body2" sx={{ color: '#666',display: 'flex', textAlign: 'left', }}>
-              Horario: {schedules}
+              Horario: {timeSchedule()}
             </Typography>
-            
+
             {/* Carreras */}
-            <BookOpenText size={24} color="#1976d2" />
-            
-            <Box  sx={{maxWidth: '100%',overflow: 'hidden'}}>
-              <Tooltip title={careers.join(', ')} arrow>
-                <Typography
-                  variant="body2"
-                  sx={{color: '#1976d2',
-                    whiteSpace: 'nowrap',
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis',
-                    display: 'block',
-                    textAlign: 'left',}}>
-                    Carreras: {careers.join(', ')}
-                </Typography>
-              </Tooltip> 
-            </Box>           
+            {programs() &&
+              <><BookOpenText size={24} color="#1976d2" />
+                <Box  sx={{maxWidth: '100%',overflow: 'hidden'}}>
+                  <Tooltip title={programs()} arrow>
+                    <Typography
+                      variant="body2"
+                      sx={{color: '#1976d2',
+                        whiteSpace: 'nowrap',
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                        display: 'block',
+                        textAlign: 'left',}}>
+                        Carreras: {programs()}
+                    </Typography>
+                  </Tooltip>
+                </Box>
+              </>
+            }
           </Box>
           </CardContent>
         </CardActionArea>
