9bf650ed67738ececf922f69aba3366274d78049
agusnarvaez <agus.narvaez@outlook.com>
2025-04-20 17:12:16 -0300

    Corrijo petición de eventos

--- src/data/services/EventService.ts
+++ src/data/services/EventService.ts
@@ -4,15 +4,16 @@ import { Response } from '../domain/Response'
 import { IEventList } from '../domain/Event'
 import axios from 'axios'
 export class EventService implements ServiceInterface {
-  baseUrl: string = `${environment.apiUrl}/courses`
+  baseUrl: string = `${environment.apiUrl}/events`
 
   async getAll(
     classroomId?: string,
     date?: Date
   ): Promise<Response<IEventList[]>> {
+    const encodedDate = encodeURIComponent(date?.toISOString() ?? '')
     const url =
       classroomId && date
-        ? `${this.baseUrl}?classroomId=${classroomId}&date=${date.toISOString()}`
+        ? `${this.baseUrl}/${classroomId}/${encodedDate}`
         : this.baseUrl
     const eventsDTOs = await axios.get<Response<IEventList[]>>(url)
     const events = eventsDTOs.data
