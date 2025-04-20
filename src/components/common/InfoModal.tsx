520c0def8d8fd67549bc39a8bb123de57cc10bcf
agusnarvaez <agus.narvaez@outlook.com>
2025-04-20 17:14:28 -0300

    Renombro Modal a InfoModal ya que colisionaba con el componente de MUI

--- src/components/common/InfoModal.tsx
+++ src/components/common/InfoModal.tsx
@@ -9,7 +9,7 @@ import {
 import React from 'react'
 import { X } from '@phosphor-icons/react'
 
-type ModalProps = {
+type InfoModalProps = {
   children: React.ReactNode // Permite múltiples hijos de cualquier tipo
   open: boolean // Propiedad para manejar el estado del Modal
   handleClose: () => void // Función para cerrar el Modal
@@ -17,13 +17,13 @@ type ModalProps = {
   subtitle?: string
 }
 
-export default function ClassInfoModal({
+export default function InfoModal({
   children,
   open,
   handleClose,
   title,
   subtitle
-}: ModalProps) {
+}: InfoModalProps) {
   return (
     <Modal
       open={open}
