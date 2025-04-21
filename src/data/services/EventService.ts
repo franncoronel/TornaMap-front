8cb5e9d8dc3475e6d5e20b5de6c564135c9f37d9
agusnarvaez <agus.narvaez@outlook.com>
2025-04-20 23:42:50 -0300

    Corrijo encodingDate

--- src/data/services/EventService.ts
+++ src/data/services/EventService.ts
@@ -1,9 +1,9 @@
 import { ServiceInterface } from './ServiceInterface'
-// import { environment } from '@/environment'
 import { Response } from '../domain/Response'
 import { IEventList } from '../domain/Event'
 import axios from 'axios'
 import { API_URL } from '@/config'
+import { format } from 'date-fns'
 
 export class EventService implements ServiceInterface {
   baseUrl: string = `${API_URL}/events`
@@ -12,7 +12,7 @@ export class EventService implements ServiceInterface {
     classroomId?: string | null,
     date?: Date
   ): Promise<Response<IEventList[]>> {
-    const encodedDate = encodeURIComponent(date?.toISOString() ?? '')
+    const encodedDate = date ? format(date, 'yyyy-MM-dd') : ''
     const url =
       classroomId && date
         ? `${this.baseUrl}/${classroomId}/${encodedDate}`
