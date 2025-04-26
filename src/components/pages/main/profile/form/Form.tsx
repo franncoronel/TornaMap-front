1ac0627b0eeb801aedda6aaeb9c09da1d773437c
Francisco Coronel <ffranncoronel@gmail.com>
2025-04-26 15:35:37 -0300

    Inicializo componente de formulario

---
+++ src/components/pages/main/profile/form/Form.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from '@mui/material'
+import '../../../interactive-page.css'
+
+export default function Form() {
+    return (
+        <Box className='interactive-page'>
+            <Typography variant="h4">
+                Formulario
+            </Typography>
+        </Box>
+    )
+}
