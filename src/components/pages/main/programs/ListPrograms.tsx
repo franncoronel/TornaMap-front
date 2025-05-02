ac67f45d68ce3770aafac77187e36b51ecbe7492
agusnarvaez <agus.narvaez@outlook.com>
2025-05-02 19:44:26 -0300

    Cambio título

--- src/components/pages/main/programs/ListPrograms.tsx
+++ src/components/pages/main/programs/ListPrograms.tsx
@@ -1,5 +1,5 @@
 import CustomCard from '@/components/common/CustomCard'
-import { Typography, Box,Grid2 } from '@mui/material'
+import { Typography, Box, Grid2 } from '@mui/material'
 import SearchBar from '@/components/common/SearchBar'
 import { IProgram } from '@/data/domain/Program'
 import NoResults from '@/components/common/NoResults'
@@ -35,26 +35,34 @@ export default function ListPrograms() {
     setOpenForm(true)
   }
 
-  const filteredPrograms = programs.filter(program =>
+  const filteredPrograms = programs.filter((program) =>
     program.name.toLowerCase().includes(searchQuery.toLowerCase())
   )
 
   return (
-    <Box sx={{ mt: 5, px: 3, overflowY: 'auto', pb: 1 }} width="100%">
-      <Typography variant="h4" gutterBottom>
+    <Box
+      sx={{ mt: 5, px: 3, overflowY: 'auto', pb: 1, gap: '1rem' }}
+      width="100%"
+    >
+      <Typography variant="h1" gutterBottom>
         Programas
       </Typography>
 
-      <Box position="sticky" top="0" zIndex="10">
+      <Box
+        position="sticky"
+        top="0"
+        zIndex="10"
+        sx={{ backgroundColor: 'white', height: 'fit-content', mb: 2 }}
+      >
         <SearchBar
           onSearch={handleSearch}
-          options={programs.map(program => program.name)}
+          options={programs.map((program) => program.name)}
         />
       </Box>
 
       <Grid2 container spacing={3} justifyContent="center">
         {filteredPrograms.length > 0 ? (
-          filteredPrograms.map(program => (
+          filteredPrograms.map((program) => (
             <CustomCard
               key={program.id}
               title={program.name}
