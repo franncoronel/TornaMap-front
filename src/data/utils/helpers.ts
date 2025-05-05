cf47bfcf38d74b2b92594974db3f406feb77ffdb
Francisco Coronel <ffranncoronel@gmail.com>
2025-05-05 19:19:16 -0300

    Agrego helpers de feature/event/crud

---
+++ src/data/utils/helpers.ts
@@ -0,0 +1,19 @@
+export const weekDayES = [
+    'LUNES',
+    'MARTES',
+    'MIÉRCOLES',
+    'JUEVES',
+    'VIERNES',
+    'SÁBADO',
+    'DOMINGO'
+  ]
+
+export const weekDayShort: Record<(typeof weekDayES)[number], string> = {
+    LUNES: 'Lun',
+    MARTES: 'Mar',
+    MIÉRCOLES: 'Mié',
+    JUEVES: 'Jue',
+    VIERNES: 'Vie',
+    SÁBADO: 'Sáb',
+    DOMINGO: 'Dom'
+  }
