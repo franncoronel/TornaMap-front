7a4035cc60b57cbd82413083eca3d5b8b672f7f7
Melody Oviedo Morales <moviedomorales@estudiantes.unsam.edu.ar>
2025-05-05 18:56:28 -0300

    Agrego propiedad centerText para alinear el texto en el botón de perfil

--- src/components/pages/main/profile/ProfileButton.tsx
+++ src/components/pages/main/profile/ProfileButton.tsx
@@ -1,11 +1,20 @@
 import { Button, ButtonProps } from '@mui/material'
 
-export const ProfileButton = (props: ButtonProps) => {
+interface ProfileButtonProps extends ButtonProps {
+  centerText?: boolean
+}
+
+export const ProfileButton = ({ centerText, ...props }: ProfileButtonProps) => {
+
   return (
     <Button
       variant="contained"
       sx={{
-        width: { xs: '90%', sm: '60%', lg: '35%', xl: '25%' }
+        width: { xs: '90%', sm: '60%', lg: '35%', xl: '25%' },
+        display: 'flex',              
+        justifyContent: centerText ? 'center' : 'flex-start',  // Cambia el alineamiento si `centerText` es true
+        alignItems: 'center',
+        textAlign: centerText ? 'center' : 'left',  // Alinea el texto al centro si `centerText` es true
       }}
       {...props}
     />
