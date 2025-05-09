f475632fa2ff76b1d0113628008271ddbecd18d2
agusnarvaez <agus.narvaez@outlook.com>
2025-05-09 17:20:58 -0300

    Corrijo chunk de vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite'
+import { defineConfig  } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(), visualizer({ open: true, gzipSize: true })],
   resolve: {
     alias: {
@@ -24,19 +25,32 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: (id) => {
-          if (id.includes('@mui/material'))           return 'mui-core'
-          if (id.includes('@mui/x-date-pickers'))     return 'mui-pickers'
-          if (id.includes('@mui/system'))             return 'mui-system'
-          if (id.includes('node_modules')) {
-            const parts = id.split('node_modules/')
-            const moduleName = parts[1].split('/')[0]
-            return `vendor-${moduleName}`
+        manualChunks(id) {
+          if (!id.includes('node_modules')) return
+
+          // Extrae el nombre del paquete
+          const pkg = id
+            .split('node_modules/')[1]
+            .split('/')[0]
+
+          // Ignora paquetes muy pequeños para que se queden en el chunk principal
+          const smallPkgs = [
+            'react-is',
+            'scheduler',      // react-scheduler
+            'react',          // puedes incluso agrupar react→vendor-react-is
+            // añade aquí cualquier otro que no quieras en su propio chunk
+          ]
+          if (smallPkgs.includes(pkg)) {
+            return     // undefined → entra en el chunk por defecto
           }
+
+          // Chunk normal para todo lo demás
+          return `vendor-${pkg}`
         }
       }
     }
   }
+
 })
 
 
