2cd792c27c4407185ffb6c529e9665cb81e905e9
agusnarvaez <agus.narvaez@outlook.com>
2025-05-09 15:11:07 -0300

    Agrego visualizer para ver cómo se distribuyen los chunks del build

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer({ open: true, gzipSize: true })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
@@ -19,5 +20,20 @@ export default defineConfig({
     coverage: {
       reporter: ['lcov', 'json', 'html', 'json-summary']
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            const parts = id.split('node_modules/')
+            const moduleName = parts[1].split('/')[0]
+            return `vendor-${moduleName}`
+          }
+        }
+      }
+    }
   }
 })
+
+
