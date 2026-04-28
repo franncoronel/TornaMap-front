fb329d59bde726bf9b780dfb4d047cd1018c6bc1
Melody Oviedo Morales <moviedomorales@estudiantes.unsam.edu.ar>
2026-04-28 02:38:54 -0300

    chore: agrego comentarios en buildingMapper

--- src/data/mapper/buildingMapper.ts
+++ src/data/mapper/buildingMapper.ts
@@ -24,24 +24,24 @@ export const normalize = (name: string) =>
 
 // Mapper principal
 export const mapBuildingsToUI = (
-  buildings: IBuildingList[]
+  buildings: IBuildingList[]  //me traigo los edificios del back
 ): UIBuilding[] => {
-  return buildings.map((b) => {
+  return buildings.map((b) => {   //itero cada edificio 
     const levels = [
-      ...new Set(b.classrooms.map((c) => c.floor))
+      ...new Set(b.classrooms.map((c) => c.floor)) //traigo los pisos de cada aula y elimino duplicados
     ]
-      .sort((a, b) => a - b)
-      .map((lvl) => ({
+      .sort((a, b) => a - b)  //ordeno los pisos de menor a mayor
+      .map((lvl) => ({  //mappeo cada piso a su formato UI 
         level: lvl,
-        text: floorToLabel(lvl),
-        path: floorToPath(lvl)
+        text: floorToLabel(lvl),  //convierto el número a un texto
+        path: floorToPath(lvl)  //convierto el número a formato URL
       }))
 
     return {
       id: b.id,
-      text: b.name,
-      path: normalize(b.name),
-      levels
+      text: b.name,   //nombre del edificio
+      path: normalize(b.name),  //nombre del edificio en formto URL
+      levels  //lista de pisos
     }
   })
 }
