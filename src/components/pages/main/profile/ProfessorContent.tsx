178bdce8a577e55e670673116cf592124d1f07f8
Joaquín Navarro <joanavarro23@hotmail.com>
2026-07-02 22:54:17 -0300

    refactor: perfil del profesor ahora trae la lista del back

--- src/components/pages/main/profile/ProfessorContent.tsx
+++ src/components/pages/main/profile/ProfessorContent.tsx
@@ -1,33 +1,46 @@
 import { IReservation } from '@/data/domain/ClassroomReservation'
 import { ProfileListEntry, ProfileListSection } from './ProfileListSection'
-
-// Data mockeada porque falta un endpoint para esto
-// GET que devuelva una lista de espacios que reservé
-const mockReservedRooms: IReservation[] = [
-  {
-    id: '1',
-    classroomName: 'CIDI - Tornavías Piso 1',
-    event: 'Cursada',
-    course: 'Telecomunicaciones y Redes',
-    courseData: ['Inscriptos: 50'],
-    schedules: 'MIÉRCOLES 18:00 - 21:00 | VIERNES 18:00 - 21:00'
-  },
-  {
-    id: '2',
-    classroomName: 'LC1 - Tornavías Piso 1',
-    event: 'Cursada',
-    course: 'Paradigmas de Programación',
-    courseData: ['Inscriptos: 36'],
-    schedules: 'MARTES 18:00 - 21:00'
-  }
-]
+import { useEffect, useState } from 'react'
+import { useLoader } from '@/context/LoaderContext'
+import { useNotification } from '@/context/NotificationContext'
+import { userService } from '@/data/services/UserService'
 
 export function ProfessorContent() {
-  //Deberia traermelas del backend, por ahora son la data mockeada
-  const reservedRooms = mockReservedRooms
+  const [reservedRooms, setReservedRooms] = useState<IReservation[]>([])
+  const { setLoader } = useLoader()
+  const { setNotificationState } = useNotification()
 
-  const handleCancel = (id: string | number) => {
-    console.log('Diste de baja tu reserva del espacio: ', id)
+  const fetchReservations = async () => {
+    try {
+      setLoader(true)
+      const res = await userService.getMyReservations()
+      setReservedRooms(res.data.data)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setLoader(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchReservations()
+  }, [])
+
+  const handleCancel = async (id: string | number) => {
+    try {
+      setLoader(true)
+      await userService.cancelReservation(id)
+      setNotificationState({
+        title: 'Reserva cancelada exitosamente',
+        type: 'success'
+      })
+      fetchReservations() // Recargamos la lista
+    } catch (error) {
+      setNotificationState({ title: 'Error al cancelar la reserva', type: 'error' })
+      console.error(error)
+    } finally {
+      setLoader(false)
+    }
   }
 
   const items: ProfileListEntry[] = reservedRooms.map((r) => ({
